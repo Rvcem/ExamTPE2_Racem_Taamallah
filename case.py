7aae73bbde4229de305427fb392958363731b8ae
racem <rataamallah@gmail.com>
2025-03-14 10:11:49 +0000

    fusion 2

--- case.py
+++ case.py
@@ -1,6 +1,9 @@
-<<<<<<< HEAD
-def somme(T):
- s=0
- for t in T: S+=t 
- return S
 
+data=[1,3,5] 
+if data:
+	print('la somme est :',sum(data))
+	print('le min est :',min(data))
+	print('le max est:',max(data)) 
+else:
+	print('dossier vide')
+>>>>>>> casee2
